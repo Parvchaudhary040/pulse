e7940a84aa5f06d3a2d936ffb3016a1174ba4a01
Parv Chaudhary <mrabh@chaudhary>
2026-06-24 22:40:43 +0530

    dashboard

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import dashboardRoutes from "./routes/dashboardRoutes";
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
 import authRoutes from "./routes/authRoutes";
@@ -26,6 +27,7 @@ app.use(express.json());
 app.use("/api/projects", projectRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+app.use("/api/dashboard", dashboardRoutes);
 
 // Health Check Route
 app.get("/", (req, res) => {
