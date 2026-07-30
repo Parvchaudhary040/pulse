86c96689fa66c1190ff29f0f28700c222249be53
Parv Chaudhary <parvchaudhary040@gmail.com>
2026-07-30 13:01:36 +0530

    updated

--- frontend/src/services/dashboardService.ts
+++ frontend/src/services/dashboardService.ts
@@ -1,8 +1,5 @@
 import api from "./api";
 
-const API_URL =
-  "http://localhost:5000/api/dashboard";
-
 export const getStats = async () => {
   const response = await api.get("/dashboard/stats");
 
